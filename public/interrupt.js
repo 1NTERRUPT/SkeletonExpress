984337bc4ba6dd128a0c87349de82e7c65466ba4
mylesforrest <mforrest@checkernet.com>
2016-01-24 12:02:04 -0500

    Add basic jquery event handling for the toggle buttons

--- public/interrupt.js
+++ public/interrupt.js
@@ -1,3 +1,15 @@
 $(function(){
 	console.log('Interrupt js loaded');
-})
+
+	$('.red').on('click', function(e){
+		e.preventDefault();
+	});
+
+	$('.green').on('click', function(e){
+		e.preventDefault();
+	});
+
+	$('.blue').on('click', function(e){
+		e.preventDefault();
+	});
+});
