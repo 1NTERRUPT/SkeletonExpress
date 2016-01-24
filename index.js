79b27bf3decc8f123d59971c37ae49a5850d0b3d
mylesforrest <mforrest@checkernet.com>
2016-01-24 11:37:03 -0500

    Add basic jade setup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res){
-	res.send('#YOLO!');
+	res.render('index', {});
 });
 
 app.listen(3000, function(){
