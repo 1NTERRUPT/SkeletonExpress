5d8828f46e215497c524a06fbcb64c362b382325
mylesforrest <mforrest@checkernet.com>
2016-01-24 11:31:08 -0500

    Add simple express webserver

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('#YOLO!');
+});
+
+app.listen(3000, function(){
+	console.log('Listening on port 3000');
+});
